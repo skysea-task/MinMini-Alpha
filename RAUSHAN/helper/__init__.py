ddbc65ca185ac160e98722f4d1fd93c2df473079
Rajesh Raki's <91279429+rajeshrakis@users.noreply.github.com>
2025-05-24 22:00:55 +0530

    __init__.py

--- RAUSHAN/helper/__init__.py
+++ RAUSHAN/helper/__init__.py
@@ -9,6 +9,6 @@ def restart():
 
 async def join(client):
     try:
-        await client.join_chat("WORLD_ALPHA")
+        await client.join_chat("HeartBeat_Muzic")
     except BaseException:
         pass
